89b8f46f808f5241148e69782271084221968f49
mohamadhaidar1012 <mohamad.haidar1012@gmail.com>
2025-05-24 13:14:23 +0300

    wishlist done

--- src/main/java/com/example/demo/controller/WishlistController.java
+++ src/main/java/com/example/demo/controller/WishlistController.java
@@ -1,4 +1,43 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.Campaign;
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.service.WishlistService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/wishlist")
 public class WishlistController {
+    @Autowired
+    private WishlistService wishlistService;
+    @Autowired
+    private UserRepository userRepository;
+    private User findUserById(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+
+    @PostMapping()
+    public ResponseEntity<String> addToWishlist(@RequestParam Long userId, @RequestParam Long campaignId){
+        wishlistService.addToWishlist(userId, campaignId);
+        return ResponseEntity.ok("Campaign Add to Wishlist");
+    }
+    @GetMapping("/list/{userId}")
+    public ResponseEntity<List<Campaign>> getWishlist(@PathVariable Long userId){
+        User user = findUserById(userId);
+        List<Campaign> wishlist = wishlistService.getWishlistForUser(user);
+        return ResponseEntity.ok(wishlist);
+    }
+    @DeleteMapping()
+    public ResponseEntity<String> deleteFromWishlist(@RequestParam Long userId, @RequestParam Long campaignId){
+        User user = findUserById(userId);
+        wishlistService.deleteFromWishlist(user,campaignId);
+        return ResponseEntity.ok("Campaign removed from your wishlist");
+    }
 }
--- src/main/java/com/example/demo/service/WishlistService.java
+++ src/main/java/com/example/demo/service/WishlistService.java
@@ -6,6 +6,7 @@ import com.example.demo.entity.Wishlist;
 import com.example.demo.repository.CampaignRepository;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.repository.WishlistRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@Transactional
 public class WishlistService {
     @Autowired
     private WishlistRepository wishlistRepository;
